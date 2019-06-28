96cf63ddace066eb4139294bf0fef215c84743d8
eunyeoul <eunyull102@naver.com>
2019-06-28 17:00:17 +0900

    뭐가 어떻게된거지??

--- app/src/main/java/com/example/myjetpackwithandroidx/view/GardenFragment.kt
+++ app/src/main/java/com/example/myjetpackwithandroidx/view/GardenFragment.kt
@@ -12,5 +12,6 @@ class GardenFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentGardenBinding.inflate(inflater, container, false)
         return binding.root
+        
     }
 }
